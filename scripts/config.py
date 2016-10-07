d2223ba984f59bac033f5b79dff60399a7c3f382
raghudeep <raghudeep.g@gmail.com>
2016-10-07 22:39:57 +0200

    minor

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,5 @@
 import sys
-CAFFE_ROOT = '../build/tmp_caffe_clone/src/CaffeUpstream/'
+CAFFE_ROOT = 'build/tmp_caffe_clone/src/CaffeUpstream/'
 sys.path.insert(0, CAFFE_ROOT + 'python')
 import caffe
 caffe.set_mode_gpu()

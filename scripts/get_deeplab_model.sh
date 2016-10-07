624e398bedb12900aec5d8fcb666b2acbd5831dd
raghudeep <raghudeep.g@gmail.com>
2016-10-07 22:31:48 +0200

    minor

--- scripts/get_deeplab_model.sh
+++ scripts/get_deeplab_model.sh
@@ -1,2 +1,2 @@
 
-wget -nc http://files.is.tue.mpg.de/vjampani/deeplab_bi_models/deeplab_coco_largefov_bi6_2_bi7_6.caffemodel -P ../models/
+wget -nc http://files.is.tue.mpg.de/vjampani/deeplab_bi_models/deeplab_coco_largefov_bi6_2_bi7_6.caffemodel -P models/
